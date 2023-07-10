9acc16cc6a30af2ce32ccee2b3148f4866b39e77
Boomaa23 <cmo93003@yahoo.com>
2023-07-10 02:07:30 -0700

    Implement cart management and approval workflow

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import os
+import re
 
 import dotenv
 from slack_bolt import App, Ack, Respond, Say
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk import WebClient
 
 import db
 
 
-CHANNEL = 'eecs-shopping-cart'
-
 dotenv.load_dotenv()
+
+CHANNEL_NAME = os.environ['CHANNEL_NAME']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+APPROVAL_THRESHOLD = 1
+
+_approve_reaction_matchers = [
+    lambda event: event['reaction'] == 'white_check_mark',
+    lambda event: event['item']['channel'] == CHANNEL_ID,
+    lambda event: event['user'] in [approver.uid for approver in db.get_approvers()]
+]
+
 app = App(token=os.environ['SLACK_BOT_TOKEN'])
 
 
@@ -29,18 +40,12 @@ def add(ack: Ack, respond: Respond, say: Say, command: dict):
     cart, part, qty = args
     user = db.User(command['user_name'], command['user_id'])
 
-    success = db.add_part(cart, part, user)
+    success = db.add_part(cart, part, qty, user)
     if not success:
-        respond(text=f'Addition of {qty} part(s) {part} to cart {cart} did not succeed.')
+        respond(text=f'Addition of {qty} part(s) {part} to cart {cart} failed. Cart has not been created.')
         return
-    text = f'<@{user.name}> added {qty} of {part} to {cart}'
-    say(channel=CHANNEL, text=text)
-
-
-# TODO
-@app.command('/sb-add-all')
-def add_all(ack: Ack, respond: Respond, say: Say, command: dict):
-    ack()
+    text = f'{user.mention()} added {qty} of {part} to {cart}'
+    say(channel=CHANNEL_NAME, text=text)
 
 
 @app.command('/sb-rm')
@@ -58,14 +63,15 @@ def rm(ack: Ack, respond: Respond, say: Say, command: dict):
 
     success = db.rm_part(cart, part, user)
     if not success:
-        respond(text=f'Removal of part {part} from cart {cart} did not succeed.')
+        respond(text=f'Removal of part {part} from cart {cart} did not succeed. '
+                     'Cart does not exist or part was not found in cart.')
         return
-    text = f'<@{user.name}> removed {part} from {cart}'
-    say(channel=CHANNEL, text=text)
+    text = f'{user.mention()} removed {part} from {cart}'
+    say(channel=CHANNEL_NAME, text=text)
 
 
 @app.command('/sb-list')
-def lst(ack: Ack, respond: Respond, say: Say, command: dict):
+def list_(ack: Ack, respond: Respond, say: Say, command: dict):
     ack()
 
     if _incorrect_channel(command, respond):
@@ -78,73 +84,218 @@ def lst(ack: Ack, respond: Respond, say: Say, command: dict):
         args.append(False)
     cart_name, send_public = args
 
-    cart_contents = db.list_cart(cart_name)
-    cart_contents_fmt = '\n'.join([f'- {q} x {p}' for p, q in cart_contents])
-    text = f'<@{command["user_name"]}> requested cart {cart_name}:\n{cart_contents_fmt}'
-    say(channel=CHANNEL, text=text) if send_public else respond(text=text)
+    text = _cart_content_fmtstr(cart_name, command["user_name"])
+    if not text:
+        respond(text=f'Listing of cart {cart_name} did not succeed. Cart does not exist.')
+        return
+    say(channel=CHANNEL_NAME, text=text) if send_public else respond(text=text)
+
 
 @app.command('/sb-create')
 def create(ack: Ack, respond: Respond, say: Say, command: dict):
     ack()
 
-    cart, user = _cart_op_args(command, respond)
+    cart, user = _single_arg(command, respond)
+    if not cart and not user:
+        return
 
     success = db.create_cart(cart)
     if not success:
-        respond(text=f'Creating cart {cart} did not succeed.')
+        respond(text=f'Creating cart {cart} did not succeed. Cart already exists.')
         return
-    text = f'<@{user.name}> created cart {cart}'
-    say(channel=CHANNEL, text=text)
+    text = f'{user.mention()} created cart {cart}'
+    say(channel=CHANNEL_NAME, text=text)
 
 
 @app.command('/sb-clear')
 def clear(ack: Ack, respond: Respond, say: Say, command: dict):
     ack()
 
-    cart, user = _cart_op_args(command, respond)
+    cart, user = _single_arg(command, respond)
 
     success = db.clear_cart(cart, user)
     if not success:
         respond(text=f'Clearing cart {cart} did not succeed.')
         return
-    text = f'<@{user.name}> cleared cart {cart}'
-    say(channel=CHANNEL, text=text)
-
-
-def _cart_op_args(command, respond):
-    if _incorrect_channel(command, respond):
-        return
-    args = _parse_args(command)
-    if len(args) != 1:
-        _incorrect_num_args(respond, 1, len(args))
-        return
-    cart = args[0]
-    user = db.User(command['user_name'], command['user_id'])
-    return cart, user
+    text = f'{user.mention()} cleared cart {cart}'
+    say(channel=CHANNEL_NAME, text=text)
 
 
 @app.command('/sb-buy')
-def buy(ack: Ack, respond: Respond, say: Say, command: dict):
+def buy(ack: Ack, respond: Respond, say: Say, command: dict, client: WebClient):
     ack()
+
+    cart, user = _single_arg(command, respond)
+    if db.get_approvals(cart) is not None:
+        respond(text=f'Approval workflow already exists for cart {cart}.')
+        return
+    approvers = ' '.join([approver.mention() for approver in db.get_approvers()])
+
+    cart_content = _cart_content_fmtstr(cart, user.name)
+    if not cart_content:
+        respond(text=f'Approval workflow for {cart} did not succeed. Cart does not exist.')
+        return
+    text = '*BEGIN PURCHASE REQUEST*\n\n' \
+           f'{cart_content}\n\n' \
+           f'Approvers (required): {approvers}\n' \
+           'Please react :white_check_mark: to approve this purchase request'
+    resp = say(channel=CHANNEL_NAME, text=text)
+    ts = resp.data['ts']
+    success = db.begin_approval(cart, ts)
+    if not success:
+        client.chat_delete(channel=CHANNEL_ID, ts=ts)
+        respond(text='Approval workflow did not succeed.')
+        return
+
+
+@app.event(event='reaction_added', matchers=_approve_reaction_matchers)
+def add_approve_reaction(ack: Ack, say: Say, event: dict, client: WebClient):
+    ack()
+
+    reaction = client.reactions_get(channel=CHANNEL_ID, timestamp=event['item']['ts'])
+    if not reaction or not reaction.data['ok']:
+        _client_post_ephemeral(client, event,
+                               'Failed getting message which reaction was applied to.')
+        return
+    message = reaction.data['message']
+    cart_matches = re.findall(r'> requested cart (.+):', message['text'])
+    if len(cart_matches) != 1:
+        # This should be silent; unrelated message
+        return
+    cart = cart_matches[0]
+    if cart not in db.database.tables():
+        _client_post_ephemeral(client, event,
+                               f'Approval failed. Cart {cart} does not exist.')
+        return
+
+    user = None
+    all_approvers = db.get_approvers()
+    for approver in all_approvers:
+        if approver.uid == event['user']:
+            user = approver
+            break
+    if not user:
+        _client_post_ephemeral(client, event,
+                               f'Approval failed. User {user.name} is not in the approvers list.')
+        return
+
+    success = db.add_approval(cart, user, event['event_ts'])
+    if not success:
+        _client_post_ephemeral(client, event,
+                               f'Approval failed. Approval for cart {cart} has not been started.')
+        return
+    text = f'{user.mention()} approved cart {cart}'
+    say(channel=CHANNEL_NAME, text=text)
+
+    approvals = db.get_approvals(cart)
+    if len(approvals) == APPROVAL_THRESHOLD:
+        cart_content = _cart_content_fmtstr(cart, user.name)
+        approvals_repr = ' '.join([db.User.from_dict(approval['user']).mention() for approval in approvals])
+        text = '*PURCHASE REQUEST APPROVED*\n\n' \
+               f'{cart_content}\n\n' \
+               f'Approved by: {approvals_repr}\n' \
+               f'Cart {cart} will be cleared.'
+        if not db.clear_cart(cart, user):
+            _client_post_ephemeral(client, event,
+                                   f'Cart {cart} could not be cleared after approval. Exiting.')
+            return
+        if not db.clear_approvals(cart, user):
+            _client_post_ephemeral(client, event,
+                                   f'Approvals list for cart {cart} could not be cleared after approval. Exiting.')
+            return
+        say(channel=CHANNEL_NAME, text=text)
 
 
 @app.event('reaction_added')
-def approve_reaction(ack: Ack, say: Say, respond: Respond, event: dict):
+def ignore_reaction_add(body, logger):
+    # All reaction additions need to be handled
+    pass
+
+
+@app.event(event='reaction_removed', matchers=_approve_reaction_matchers)
+def rm_approve_reaction(ack: Ack, say: Say, respond: Respond, event: dict, client: WebClient):
+    ack()
+    # TODO finish
+
+
+@app.event('reaction_removed')
+def ignore_reaction_rm(body, logger):
+    # All reaction removals need to be handled
+    pass
+
+
+@app.command('/sb-add-approver')
+def add_approver(ack: Ack, respond: Respond, say: Say, command: dict):
     ack()
 
-    if _incorrect_channel(event, respond) or \
-            event['reaction'] != 'white_check_mark' or \
-            event['user'] not in [user.uid for user in db.get_approvers()]:
+    approver_str, user = _single_arg(command, respond)
+    approver = db.User.from_str(approver_str)
+
+    #TODO uncomment after testing
+    # if approver == user:
+    #     respond(text='Approver cannot be yourself. Ask someone else to add you.')
+    #     return
+
+    success = db.add_approver(approver, user)
+    if not success:
+        respond(text=f'Addition of approver user {approver.name} did not succeed')
         return
-    #TODO finish
-    say(channel='eecs-shopping-cart', text='testing')
+    text = f'{user.mention()} added {approver.mention()} to the approver list'
+    say(channel=CHANNEL_NAME, text=text)
+
+
+@app.command('/sb-rm-approver')
+def rm_approver(ack: Ack, respond: Respond, say: Say, command: dict):
+    ack()
+
+    approver_str, user = _single_arg(command, respond)
+    approver = db.User.from_str(approver_str)
+
+    if approver not in db.get_approvers():
+        respond(text=f'User {approver.name} is not in the approver list.')
+        return
+
+    success = db.rm_approver(approver)
+    if not success:
+        respond(text=f'Removing {approver.name} from approvers list failed. Approver not found in list.')
+        return
+    text = f'{user.mention()} removed {approver.mention()} from the approvers list'
+    say(channel=CHANNEL_NAME, text=text)
+
+
+@app.command('/sb-help')
+def help_(ack: Ack, respond: Respond, say: Say, command: dict):
+    ack()
+    # TODO finish
+
+
+def _client_post_ephemeral(client, event, text):
+    client.chat_postEphemeral(channel=CHANNEL_ID, user=event['user'], text=text)
+
+def _single_arg(command, respond):
+    if _incorrect_channel(command, respond):
+        return None, None
+    args = _parse_args(command)
+    if len(args) != 1:
+        _incorrect_num_args(respond, 1, len(args))
+        return None, None
+    user = db.User(command['user_name'], command['user_id'])
+    return args[0], user
+
+
+def _cart_content_fmtstr(cart_name, user_name):
+    if cart_name not in db.database.tables():
+        return None
+    cart_contents = db.list_cart(cart_name)
+    cart_contents_fmt = '\n'.join([f'- {q} x {p} ({u.name})' for p, q, u in cart_contents])
+    return f'<@{user_name}> requested cart {cart_name}:\n{cart_contents_fmt}'
 
 
 def _incorrect_channel(command, respond) -> bool:
     actual_channel = command['channel_name']
-    is_incorrect = actual_channel != CHANNEL
+    is_incorrect = actual_channel != CHANNEL_NAME
     if is_incorrect:
-        respond(text=f'Incorrect channel. Expected {CHANNEL}, got {actual_channel}.')
+        respond(text=f'Incorrect channel. Expected {CHANNEL_NAME}, got {actual_channel}.')
     return is_incorrect
 
 
--- db.py
+++ db.py
@@ -1,52 +1,159 @@
-import os
+import tinydb
+
+
+database = tinydb.TinyDB('db.json')
 
 
 class User:
-    def __init__(self, name, uid):
+    def __init__(self, name: str, uid: str):
         self.name = name
         self.uid = uid
 
+    @classmethod
+    def from_str(cls, escaped_str: str):
+        parts = escaped_str[2:-1].split('|')
+        return cls(name=parts[1], uid=parts[0])
 
-def add_part(cart, part, user: User) -> bool:
-    pass
+    @classmethod
+    def from_dict(cls, mapping: dict):
+        return cls(name=mapping['name'], uid=mapping['uid'])
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'uid': self.uid
+        }
+
+    def mention(self) -> str:
+        return f'<@{self.name}>'
+
+    def __eq__(self, other):
+        return self.name == other.name and \
+               self.uid == other.uid
+
+    def __str__(self):
+        return f'User(name={self.name}, id={self.uid})'
+
+    def __repr__(self):
+        return self.__str__()
 
 
-def rm_part(cart, part, user: User) -> bool:
-    pass
+def add_part(cart_name: str, part: str, qty: str, user: User) -> bool:
+    if cart_name not in database.tables():
+        return False
+    cart = database.table(cart_name)
+    cart.insert({
+        'part': part,
+        'qty': qty,
+        'user': user.to_dict()
+    })
+    return True
 
 
-def list_cart(cart) -> list:
-    # TODO replace with actual
-    return [["link1", 100], ["link2", 200], ["link3", 300]]
+def rm_part(cart_name: str, part, user: User) -> bool:
+    if cart_name not in database.tables():
+        return False
+    # TODO keep track of who removed things
+    cart = database.table(cart_name)
+    ids = cart.remove(tinydb.Query().part == part)
+    return len(ids) != 0
 
 
-def create_cart(cart) -> bool:
-    pass
+def list_cart(cart_name: str) -> list[tuple]:
+    cart = database.table(cart_name)
+    parts = cart.all()
+    return [(part['part'], part['qty'], User.from_dict(part['user'])) for part in parts if part]
 
 
-def clear_cart(cart, user: User) -> bool:
-    pass
+def create_cart(cart_name: str) -> bool:
+    if cart_name in database.tables():
+        return False
+    # On the backend, this creates a table if none existed before
+    table = database.table(cart_name)
+    # This is required, otherwise the table is not serialized
+    table.insert({})
+    return True
 
 
-def add_approver(user: User) -> bool:
-    pass
+def clear_cart(cart_name: str, user: User) -> bool:
+    if cart_name not in database.tables():
+        return False
+    # Drop and re-create is the easiest way to clear
+    # Makes a new table ID, but this is OK since
+    # nothing depends on/uses the table ID
+    database.drop_table(cart_name)
+    database.table(cart_name)
+    return True
 
 
-def rm_approver(user: User) -> bool:
-    pass
+def add_approver(approver: User, user: User) -> bool:
+    approvers = database.table('approvers')
+    approvers.insert({
+        'approver': approver.to_dict(),
+        'user': user.to_dict()
+    })
+    return True
+
+
+def rm_approver(approver: User) -> bool:
+    approvers = database.table('approvers')
+    matches = approvers.search(tinydb.Query().approver == approver)
+    if len(matches) != 1:
+        return False
+    approvers.remove(doc_ids=[matches[0].doc_id])
+    return True
 
 
 def get_approvers() -> list[User]:
-    # TODO change after testing
-    return [User(os.environ['TESTING_USER_NAME'], os.environ['TESTING_USER_ID'])]
+    table = database.table('approvers')
+    approvers = table.all()
+    return [User.from_dict(approver['approver']) for approver in approvers]
 
 
-def add_approval(cart, user: User) -> bool:
+def begin_approval(cart_name: str, ts: str) -> bool:
+    if cart_name not in database.tables():
+        return False
+    approvals = database.table('approvals')
+    approvals.insert({
+        'cart': cart_name,
+        'ts': ts,
+        'approvals': []
+    })
+    return True
+
+
+def get_approvals(cart_name: str):
+    if cart_name not in database.tables():
+        return None
+    approval_table = database.table('approvals')
+    matches = approval_table.search(tinydb.Query().cart == cart_name)
+    if len(matches) != 1:
+        return None
+    return matches[0]['approvals']
+
+
+def add_approval(cart_name: str, user: User, ts: str) -> bool:
+    cart_approvals = get_approvals(cart_name)
+    # OK for approvals to be empty, but not None
+    if cart_approvals is None:
+        return False
+    approval = {
+        'user': user.to_dict(),
+        'ts': ts
+    }
+    cart_approvals.append(approval)
+    return True
+
+
+def rm_approval(cart_name: str, user: User) -> bool:
     pass
 
 
-def clear_approvals(cart, user: User) -> bool:
-    pass
+def clear_approvals(cart_name: str, user: User) -> bool:
+    approval_table = database.table('approvals')
+    matches = approval_table.search(tinydb.Query().cart == cart_name)
+    approval_table.remove(doc_ids=[match.doc_id for match in matches])
+    return True
 
 
 if __name__ == '__main__':
